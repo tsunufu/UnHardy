2da0ec47ce1c2ff0daca9e16eb8f6289bd415c0b
tsunufu <tsunufu@icloud.com>
2022-09-16 18:05:58 +0900

    データベース設計

---
+++ UnHardy/Add.swift
@@ -0,0 +1,13 @@
+//
+//  Add.swift
+//  UnHardy
+//
+//  Created by ryo on 2022/09/16.
+//
+
+import Foundation
+import RealmSwift
+
+class Add: Object {
+    @objc dynamic var image: String = ""
+}
---
+++ UnHardy/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  UnHardy
+//
+//  Created by ryo on 2022/09/16.
+//
+
+import Foundation
+import RealmSwift
+
+class User: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var icon: String = ""
+    @objc dynamic var goal: String = ""
+    
+}
